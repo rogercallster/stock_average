f076b440bead440696ab2fb4fa0f9f95776e5761
asaran <rogercallster@gmail.com>
2025-08-15 16:13:11 -0700

    first commit scaffolding

---
+++ src/main/java/com/stock/stock_average/StockAverageApplication.java
@@ -0,0 +1,13 @@
+package com.stock.stock_average;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAverageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockAverageApplication.class, args);
+    }
+
+}
